62859013b2593a32e7d237e484201479d40162a2
aklilu <akliludin@gmail.com>
2021-11-15 20:42:25 +0100

    update

--- pipeline/lib/flood_model/secrets.py
+++ pipeline/lib/flood_model/secrets.py
@@ -3,7 +3,7 @@ import os
 #API_SERVICE_URL = os.environ["API_SERVICE_URL"]
 #IBF_API_URL=os.environ["IBF_API_URL"]
 ADMIN_LOGIN = os.environ.get("ADMIN_LOGIN")
-ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
+#ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
 ##glofas
 GLOFAS_USER = os.environ["GLOFAS_USER"]
 GLOFAS_API_KEY=os.environ["GLOFAS_API_KEY"]
@@ -16,23 +16,29 @@ GOOGLE_DRIVE_DATA_URL=os.environ["GOOGLE_DRIVE_DATA_URL"]
 DATALAKE_STORAGE_ACCOUNT_NAME = os.environ["DATALAKE_STORAGE_ACCOUNT_NAME"]
 DATALAKE_STORAGE_ACCOUNT_KEY = os.environ["DATALAKE_STORAGE_ACCOUNT_KEY"]
 DATALAKE_API_VERSION = os.environ["DATALAKE_API_VERSION"]
-
+UGA_PASSWORD=os.environ["UGA_PASSWORD"]
+ZMB_PASSWORD=os.environ["ZMB_PASSWORD"]
+ETH_PASSWORD=os.environ["ETH_PASSWORD"]
+KEN_PASSWORD=os.environ["KEN_PASSWORD"]
 # COUNTRY SETTINGS
 SETTINGS_SECRET = {
     "ZMB": {
         "IBF_API_URL":'https://ibf-zambia.510.global/api/',
+        "PASSWORD": os.environ["ZMB_PASSWORD"],
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False
     },
     "UGA": {
         "IBF_API_URL":'https://ibf-uganda.510.global/api/',
+        "PASSWORD": os.environ["UGA_PASSWORD"],
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False
     },
     "KEN": {
         "IBF_API_URL":'https://ibf-kenya.510.global/api/',
+        "PASSWORD": os.environ["KEN_PASSWORD"],
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False,
@@ -40,6 +46,7 @@ SETTINGS_SECRET = {
     },
     "ETH": {
         "IBF_API_URL":'https://ibf-ethiopia.510.global/api/',
+        "PASSWORD": os.environ["ETH_PASSWORD"],
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False
