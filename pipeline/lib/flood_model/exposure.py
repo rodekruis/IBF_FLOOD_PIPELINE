c61bccdaaa517b18a68382914cf0b4088adde30a
aklilu <akliludin@gmail.com>
2022-08-08 20:08:33 +0200

    update

--- pipeline/lib/flood_model/exposure.py
+++ pipeline/lib/flood_model/exposure.py
@@ -33,8 +33,8 @@ class Exposure:
         self.PIPELINE_INPUT_COD=PIPELINE_INPUT+'cod/'
         self.POPULATION_PATH= os.path.join(self.PIPELINE_INPUT_COD,f"{countryCodeISO3}_{self.admin_level}_population.json") 
 
-        self.ADMIN_AREA_GDF_TMP_PATH = os.path.join(PIPELINE_OUTPUT,"admin-areas_TMP.geojson")  #s.geojson', driver='GeoJSON')
-        #self.ADMIN_AREA_GDF_TMP_PATH = PIPELINE_OUTPUT+"admin-areas_TMP.shp"   
+        #self.ADMIN_AREA_GDF_TMP_PATH = os.path.join(PIPELINE_OUTPUT,"admin-areas_TMP.geojson")  #s.geojson', driver='GeoJSON')
+        self.ADMIN_AREA_GDF_TMP_PATH = PIPELINE_OUTPUT+"admin-areas_TMP.shp"   
         self.EXPOSURE_DATA_SOURCES = SETTINGS[countryCodeISO3]['EXPOSURE_DATA_SOURCES']
         
         self.levels = SETTINGS[countryCodeISO3]['levels']
@@ -173,8 +173,8 @@ class Exposure:
             except ValueError:
                 logger.info('Rasters do not overlap')
         #self.ADMIN_AREA_GDF_ADM_LEL_=self.ADMIN_AREA_GDF_ADM_LEL.query(f'adminLevel == {adm_level}')
-        #self.ADMIN_AREA_GDF.to_file(self.ADMIN_AREA_GDF_TMP_PATH)
-        self.ADMIN_AREA_GDF.to_file(self.ADMIN_AREA_GDF_TMP_PATH,driver='GeoJSON')
+        self.ADMIN_AREA_GDF.to_file(self.ADMIN_AREA_GDF_TMP_PATH)
+        #self.ADMIN_AREA_GDF.to_file(self.ADMIN_AREA_GDF_TMP_PATH,driver='GeoJSON')
         stats = self.calcStatsPerAdmin(indicator, disasterExtentShapes, rasterValue)
         return stats
 
