c40865d7b1602d77a6d05eb38daf5b59555f5323
aklilu <akliludin@gmail.com>
2022-11-17 09:45:50 +0100

    for PHL limit pipeline run to areas covered by the EAP, to make it faster

--- pipeline/lib/flood_model/floodExtent.py
+++ pipeline/lib/flood_model/floodExtent.py
@@ -58,19 +58,34 @@ class FloodExtent:
             #if pcode in Areas_With_GlofasStation:
             
             #If trigger, find the right flood extent and clip it for the area and save it
-            trigger = rows['fc_trigger']
-            if trigger == 1:
-                return_period = rows['fc_rp_flood_extent'] 
-                input_raster = self.inputPath + self.countryCodeISO3 + '_flood_' +str(int(return_period))+'year.tif'
+            if self.countryCodeISO3 =='PHL':
+                if pcode in self.Areas_With_GlofasStation:
+                    logger.info(f'procssing {pcode}')                
+                    #If trigger, find the right flood extent and clip it for the area and save it
+                    trigger = rows['fc_trigger']
+                    if trigger == 1:
+                        return_period = rows['fc_rp_flood_extent'] 
+                        input_raster = self.inputPath + self.countryCodeISO3 + '_flood_' +str(int(return_period))+'year.tif'
+                    else:
+                        input_raster = self.inputPath + self.countryCodeISO3 + '_flood_empty.tif'
+                    out_image, out_meta = self.clipTiffWithShapes(input_raster, dist_coords)                   
+
+                    with rasterio.open(self.outputPathAreas+ 'pcode_' + str(pcode) + ".tif", "w", **out_meta) as dest:
+                        dest.write(out_image)                       
             else:
-                input_raster = self.inputPath + self.countryCodeISO3 + '_flood_empty.tif'
+                trigger = rows['fc_trigger']
+                if trigger == 1:
+                    return_period = rows['fc_rp_flood_extent'] 
+                    input_raster = self.inputPath + self.countryCodeISO3 + '_flood_' +str(int(return_period))+'year.tif'
+                else:
+                    input_raster = self.inputPath + self.countryCodeISO3 + '_flood_empty.tif'
 
-            out_image, out_meta = self.clipTiffWithShapes(input_raster, dist_coords)
-            
+                out_image, out_meta = self.clipTiffWithShapes(input_raster, dist_coords)
+                
 
-            with rasterio.open(self.outputPathAreas+ 'pcode_' + str(pcode) + ".tif", "w", **out_meta) as dest:
-                dest.write(out_image)
-            logger.info(f"flood extent file written for {pcode}")
+                with rasterio.open(self.outputPathAreas+ 'pcode_' + str(pcode) + ".tif", "w", **out_meta) as dest:
+                    dest.write(out_image)
+                logger.info(f"flood extent file written for {pcode}")
 
         #Merge all clipped flood extents back together and Save
         mosaic, out_meta = self.mergeRasters()
